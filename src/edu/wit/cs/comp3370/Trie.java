f3170bbd3d4ac44f98890534683b8257b046c443
end user <kreimendahlf@wit.edu>
2017-06-15 00:24:49 -0400

    added skeleton code

---
+++ src/edu/wit/cs/comp3370/Trie.java
@@ -0,0 +1,30 @@
+package edu.wit.cs.comp3370;
+
+/* Implements a trie data structure 
+ * 
+ * Wentworth Institute of Technology
+ * COMP 3370
+ * Lab Assignment 6 solution
+ * 
+ */
+
+public class Trie extends Speller {
+
+	@Override
+	public void insertWord(String s) {
+		// TODO Implement this method
+	}
+
+	@Override
+	public boolean contains(String s) {
+		// TODO Implement this method
+		return false;
+	}
+
+	@Override
+	public String[] getSugg(String s) {
+		// TODO Implement this method
+		return null;
+	}
+
+}
